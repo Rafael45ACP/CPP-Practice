cd00682998d4e7ef6e85103b4b24af638728d890
Rafael45ACP <rafaelgriego1911@gmail.com>
2026-02-26 17:49:17 +0800

    First

---
+++ CPP Practice/2d array.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+string animal[][2] = {{"dog", "cat"},
+                      {"bunny", "wolf"}};
+
+    cout << animal[0][1] << " ";
+    return 0;
+}
---
+++ CPP Practice/_.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int numrow = 5;
+
+int numcol = 5;
+
+for (int r = 0; r < numrow; r++) { 
+
+for
+
+(int c = 0; c < numcol; c++) {
+
+
+cout << "*";
+
+
+}
+
+cout << endl;
+
+
+}
+
+return 0;
+
+}
