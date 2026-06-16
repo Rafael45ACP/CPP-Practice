bd25813f601d88f1d0306d4a69c04d41b6bc9c24
Rafael45ACP <rafaelgriego1911@gmail.com>
2026-06-16 23:29:58 +0000

    added start money and bet functions

--- blackjack.cpp
+++ blackjack.cpp
@@ -4,12 +4,50 @@
 
 using namespace std;
 
-int main(){
+int getStart();
+int getBet(int x);
 
-srand(time(0))
-int userc;
-int compc;
+int main()
+{
+
+    srand(time(0));
+
+    int random = rand() % 11 + 1;
+
+    int userc;
+    int compc;
+
+    int startM = 0;
+
+    int bet = 0;
+
+    startM = getStart();
+    bet = getBet(startM);
+    
+
+    userc = random + random;
+
+    cout << userc;
 
 
-return 0;
+
+    return 0;
+}
+
+int getStart(){
+    int x;
+    do {
+        cout << "What is your starting money?\n";
+        cin >> x;
+    } while(x <= 0);
+    return x;
+}
+
+int getBet(int x){
+    int y;
+    do {
+        cout << "How much do you want to bet?\n";
+        cin >> y;
+    } while(y <= 0 || y > x);
+    return y;
 }
